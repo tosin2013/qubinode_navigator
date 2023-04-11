0f32c53dd539e985245470f719fb2511212d05e0
Tosin Akinosho <takinosh@redhat.com>
2023-04-11 14:32:23 -0400

    updating code

--- inventories/equinix/check_env.py
+++ inventories/equinix/check_env.py
@@ -4,8 +4,6 @@ import sys
 if not os.environ.get('SSH_PASSWORD'):
     print("Environment variable SSH_PASSWORD is not set. Exiting.", file=sys.stderr)
     sys.exit(1)
-else:
-    print("SSH_PASSWORD is set."+ os.environ.get('SSH_PASSWORD'))
 
 if not os.environ.get('SSH_HOST'):
     print("Environment variable SSH_HOST is not set. Exiting.", file=sys.stderr)
