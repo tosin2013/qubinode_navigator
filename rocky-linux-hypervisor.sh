9bf0c8a7ed4ef96f616ff0aa6efa39d401090afb
Tosin Akinosho <takinosh@redhat.com>
2023-06-10 11:10:44 -0400

    another update

--- rocky-linux-hypervisor.sh
+++ rocky-linux-hypervisor.sh
@@ -338,7 +338,7 @@ function confiure_lvm_storage(){
     then 
         curl -OL https://raw.githubusercontent.com/tosin2013/qubinode_navigator/main/dependancies/equinix-rocky/configure-lvm.sh
         mv configure-lvm.sh /tmp/configure-lvm.sh
-        chmod +x configure-lvm.sh
+        sudo chmod +x /tmp/configure-lvm.sh
     fi 
     /tmp/configure-lvm.sh || exit 1
 }
