a36f1d46d6df643b390899500ab20b21fa5c7d5a
Tosin Akinosho <takinosh@redhat.com>
2024-07-09 18:31:31 -0400

    Refactor bash aliases and update yq dependency to v4.44.2

--- bash-aliases/functions.sh
+++ bash-aliases/functions.sh
@@ -4,7 +4,7 @@
 function dependency_check() {
     if ! yq -v  &> /dev/null
     then
-        VERSION=v4.34.1
+        VERSION=v4.44.2
         BINARY=yq_linux_amd64
         sudo wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY} -O /usr/bin/yq &&\
         sudo chmod +x /usr/bin/yq
