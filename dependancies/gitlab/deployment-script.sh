4c174c9617cdcc070a5e0412cac8af69f8968230
Tosin Akinosho <takinosh@redhat.com>
2024-08-10 20:14:00 -0400

    chore: Add deployment scripts for GitLab and Route53

---
+++ dependancies/gitlab/deployment-script.sh
@@ -0,0 +1,58 @@
+#!/bin/bash 
+
+if [ ! -f .env ]; then
+  echo "Environment file not found"
+  exit 1
+else
+    source .env
+fi
+
+if [ ! -f /tmp/requirements.yml ]; then
+  cat >/tmp/requirements.yml<<EOF
+---
+collections:
+- containers.podman
+- community.crypto
+- ansible.posix
+EOF
+
+if [ ! -d /etc/ansible/roles/podman-gitlab-server-role ];
+then
+  git clone https://github.com/tosin2013/ansible-podman-gitlab-server-role.git
+  cd /opt/ansible-podman-gitlab-server-role
+  cp -r podman-gitlab-server-role /etc/ansible/roles/
+  cp /opt/ansible-podman-gitlab-server-role/playbooks/gitlab-mgmt.yml $HOME 
+fi
+GILAB_SERVICE_ACCOUNT=gitlab
+cat > /etc/ansible/roles/podman-gitlab-server-role/defaults/main.yml <<EOF
+---
+# Username Variables
+gitlab_service_account: ${GILAB_SERVICE_ACCOUNT}
+gitlab_service_account_uid: 1001
+
+# Container Image Variables
+gitlab_server_image_name: gitlab/gitlab-ce:latest
+
+# Container Name Variables
+gitlab_server_name: gitlab_server
+
+# GitLab Hostname
+gitlab_server_hostname: gitlab
+
+# Domain
+domain: '${DOMAIN}'
+
+# Podman Ports
+gitlab_container_ssl_port: '8443:8443/tcp'
+gitlab_gui_ssl_port: '8443'
+gitlab_container_ssh_port: '2222:22/tcp'
+
+# FirewallD Ports
+gitlab_firewall_ssl_port: '8443/tcp'
+gitlab_firewall_ssh_port: '2222/tcp'
+
+# GitLab Container Specific Variables
+gitlab_server_restart_policy: always
+EOF
+
+ansible-playbook gitlab-mgmt.yml
---
+++ dependancies/route53/deployment-script.sh
@@ -0,0 +1,61 @@
+#!/bin/bash 
+
+if [ ! -f .env ]; then
+  echo "Environment file not found"
+  exit 1
+else
+    source .env
+fi
+
+if [ ! -f /tmp/requirements.yml ]; then
+  cat >/tmp/requirements.yml<<EOF
+---
+collections:
+- amazon.aws
+- community.general
+- ansible.posix
+roles: 
+- name: ansible_role_update_ip_route53
+    src: https://github.com/tosin2013/ansible-role-update-ip-route53.git
+    version: master
+EOF
+fi
+/usr/local/bin/ansiblesafe -f "/opt/qubinode_navigator/inventories/${INVENTORY}/group_vars/control/vault.yml" -o 1
+AWS_ACCESS_KEY=$(yq eval '.aws_access_key' "/opt/qubinode_navigator/inventories/${INVENTORY}/group_vars/control/vault.yml")
+AWS_SECRET_KEY=$(yq eval '.aws_secret_key' "/opt/qubinode_navigator/inventories/${INVENTORY}/group_vars/control/vault.yml")
+IP_ADDRESS=$(hostname -I | awk '{print $1}')
+VERBOSE_LEVEL="-v"
+ACTION="create"
+/usr/local/bin/ansiblesafe -f "/opt/qubinode_navigator/inventories/${INVENTORY}/group_vars/control/vault.yml" -o 2
+
+# Ensure required environment variables are set
+: "${ZONE_NAME:?Environment variable ZONE_NAME is required}"
+: "${GUID:?Environment variable GUID is required}"
+: "${ACTION:?Environment variable ACTION is required}"
+: "${VERBOSE_LEVEL:?Environment variable VERBOSE_LEVEL is required}"
+
+
+ansible-galaxy install -r /tmp/requirements.yml --force -vv
+pip3 install boto3 botocore
+
+cat >/tmp/playbook.yml<<EOF
+- name: Populate OpenShift DNS Entries
+  hosts: localhost
+  connection: local
+  become: yes
+
+  vars:
+  - update_ip_r53_aws_access_key:  ${AWS_ACCESS_KEY}
+  - update_ip_r53_aws_secret_key: ${AWS_SECRET_KEY}
+  - use_public_ip: true
+  - private_ip: "${IP_ADDRESS}"
+  - update_ip_r53_records:
+    - zone: ${ZONE_NAME}
+      record: cockpit.${GUID}.${ZONE_NAME}
+  roles:
+  - ansible_role_update_ip_route53
+EOF
+
+if [ "${ACTION}" != "delete" ]; then 
+  ansible-playbook  /tmp/playbook.yml ${VERBOSE_LEVEL} || exit $?
+fi
