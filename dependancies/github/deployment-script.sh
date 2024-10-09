b9e24b1c11d99124b8d34a3e113c96cdf0925065
Tosin Akinosho <takinosh@redhat.com>
2024-10-09 11:28:11 -0400

    updating code

--- dependancies/github/deployment-script.sh
+++ dependancies/github/deployment-script.sh
@@ -50,11 +50,11 @@ tar xzf actions-runner-linux-x64-$RUNNER_VERSION.tar.gz
 # Create the runner and start the configuration experience
 echo "Configuring runner..."
 echo ./config.sh --unattended --url https://github.com/tosin2013/kcli-pipelines --token "$KCLI_PIPELINES_RUNNER_TOKEN" --labels "self-hosted,Linux,X64,${GUID}-github-runner" --name "${GUID}-github-runner" --replace 
-nohup ./config.sh --unattended --url https://github.com/tosin2013/kcli-pipelines --token "$KCLI_PIPELINES_RUNNER_TOKEN" --labels "self-hosted,Linux,X64,${GUID}-github-runner" --name "${GUID}-github-runner" --replace || exit $?
+nohup ./config.sh --unattended --url https://github.com/tosin2013/kcli-pipelines --token "$KCLI_PIPELINES_RUNNER_TOKEN" --labels "self-hosted,Linux,X64,${GUID}-github-runner" --name "${GUID}-github-runner" --replace & || exit $?
 echo "Runner configured!"
 
 # Start the runner as a background job
-nohup ./run.sh | tee /tmp/runner.log 
+nohup ./run.sh & | tee /tmp/runner.log 
 
 # Log the process ID
 echo "Runner started with PID \$!"
